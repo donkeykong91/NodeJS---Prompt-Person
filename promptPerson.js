315a22addbebf19d780bd79561764583fc384e2d
donkeykong91 <darylnator@gmail.com>
2018-06-29 06:39:41 -0700

    Asking question and logging out answer

---
+++ promptPerson.js
@@ -0,0 +1,7 @@
+var readline = require('readline');
+
+var rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.question("What is the name of a real person?", function (answer) {
+  console.log(answer);
+});
