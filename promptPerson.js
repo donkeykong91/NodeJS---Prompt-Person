4cdd573b0257e746414c90b1dfa4b1218ac442ec
donkeykong91 <darylnator@gmail.com>
2018-06-30 05:23:14 -0700

    Added condition to check whether to exit prompt or not, added another event on close to display all the sayings the person said

--- promptPerson.js
+++ promptPerson.js
@@ -14,8 +14,17 @@ rl.question("What is the name of a real person?", function (answer) {
   rl.prompt();
 
   rl.on('line', function (saying) {
-    rl.setPrompt(`What else would ${realPerson.name} say? ('exit' to leave) `);
+    realPerson.sayings.push(saying.trim());
 
-    rl.prompt();
+    if (saying.toLowerCase().trim() === 'exit') {
+      rl.close();
+    } else {
+      rl.setPrompt(`What else would ${realPerson.name} say? ('exit' to leave) `);
+      rl.prompt();
+    }
   });
+});
+
+rl.on('close', function () {
+  console.log("%s is a real person that says %j", realPerson.name, realPerson.sayings);
 });
