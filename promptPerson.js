1a33bcb62c47a63ff2ef0acf041ffc997b1277e5
donkeykong91 <darylnator@gmail.com>
2018-06-30 04:24:47 -0700

    Added prompt and events to respond

--- promptPerson.js
+++ promptPerson.js
@@ -1,7 +1,19 @@
 var readline = require('readline');
-
 var rl = readline.createInterface(process.stdin, process.stdout);
 
+var realPerson = {
+  name: '';
+  sayings: [];
+};
+
 rl.question("What is the name of a real person?", function (answer) {
-  console.log(answer);
+  realPerson.name = answer;
+
+  rl.setPrompt(`What would ${realPerson.name} say? `);
+
+  rl.prompt();
+
+  rl.on('line', function (saying) {
+    console.log(saying.trim());
+  });
 });
